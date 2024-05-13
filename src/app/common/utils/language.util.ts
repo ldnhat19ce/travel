3361b722a25b2648bff3f8b6a69059f0a6dfa24c
nhatld <nhatld.dev@gmail.com>
2024-05-13 16:26:12 +0700

    add booking

--- src/app/common/utils/language.util.ts
+++ src/app/common/utils/language.util.ts
@@ -3,7 +3,6 @@ import { LocalStorageService } from "../services/local-storage.service";
 
 export class LanguageUtil {
 
-
     static getLanguage(localStorageService: LocalStorageService) {
         let currentLang = localStorageService.getItem(CommonConstant.LOCAL_CURRENT_LANG, "vn");
         return currentLang;
