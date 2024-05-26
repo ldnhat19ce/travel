bb885f2ebdde0bc7bb07ae70624fd588d13f480c
nhatld <nhatld.dev@gmail.com>
2024-05-26 22:18:56 +0700

    update post

--- src/app/common/pages/application/post/post-routing.module.ts
+++ src/app/common/pages/application/post/post-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostComponent } from './post.component';
+import { BookingFireworkComponent } from './booking-firework/booking-firework.component';
 
 const routes: Routes = [
     { path: ':id/:content', component: PostComponent },
+    { path: '12', component: BookingFireworkComponent },
 ];
 
 @NgModule({
