f3bdd97e86a80cd90feaefa562e54ea058d30f8a
= <=>
2024-06-12 22:27:52 +0700

    update image url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    apiUrl: "http://103.162.20.137:8080/api/v1",
+    aapiUrl: 'https://api.herotraveldn.com/api/v1',
     keyEncrypt: "KifI0dJ[Ok8$431Trpxs:KN%0Z7h]vTl",
-    imgUrl: "http://103.162.20.137"
+    imgUrl: "https://cdn.herotraveldn.com"
 };
