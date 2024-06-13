d326ca5f0d400d0f2821f2eec4d83a1edc319689
= <=>
2024-06-13 07:14:14 +0700

    fix error env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    aapiUrl: 'https://api.herotraveldn.com/api/v1',
+    apiUrl: 'https://api.herotraveldn.com/api/v1',
     keyEncrypt: "KifI0dJ[Ok8$431Trpxs:KN%0Z7h]vTl",
     imgUrl: "https://cdn.herotraveldn.com"
 };
