9c9a3277fa574c7bab4ae314d366a902f33ed50b
= <=>
2024-07-15 14:43:22 +0700

    add swiper

---
+++ src/app/common/pages/application/checkout/checkout-product/checkout-product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-checkout-product',
+    standalone: true,
+    imports: [],
+    templateUrl: './checkout-product.component.html',
+    styleUrl: './checkout-product.component.scss',
+})
+export class CheckoutProductComponent {}
--- src/app/common/pages/application/home/home.component.ts
+++ src/app/common/pages/application/home/home.component.ts
@@ -1,5 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { SeoService } from '../../../services/seo.service';
+import { isPlatformBrowser } from '@angular/common';
+import Swiper from 'swiper';
+import { Autoplay, Pagination } from 'swiper/modules';
 
 @Component({
     selector: 'app-home',
@@ -8,17 +11,59 @@ import { SeoService } from '../../../services/seo.service';
 })
 export class HomeComponent implements OnInit {
     private _seoService = inject(SeoService);
+    private _platformId = inject(PLATFORM_ID);
+
+    isBrowser: boolean = false;
 
     ngOnInit(): void {
-        this._seoService.setMetaTitle("Herotraveldn - Dịch vụ du lịch Huế - Đà Nẵng - Hội An");
-        this._seoService.setMetaDescription("Herotraveldn Chuyên tổ chức các Tour du lịch Miền Trung, Du lịch Đà Nẵng, Tour Du lịch hằng ngày Bà Nà, Hội An, Cù Lao Chàm.");
-        this._seoService.setMetaOgTitle("Herotraveldn - Dịch vụ du lịch Huế - Đà Nẵng - Hội An");
-        this._seoService.setMetaOgDescription("Herotraveldn Chuyên tổ chức các Tour du lịch Miền Trung, Du lịch Đà Nẵng, Tour Du lịch hằng ngày Bà Nà, Hội An, Cù Lao Chàm.");
-        this._seoService.setMetaTwitterTitle("Herotraveldn - Dịch vụ du lịch Huế - Đà Nẵng - Hội An");
-        this._seoService.setMetaTwitterDescription("Herotraveldn Chuyên tổ chức các Tour du lịch Miền Trung, Du lịch Đà Nẵng, Tour Du lịch hằng ngày Bà Nà, Hội An, Cù Lao Chàm.");
-        this._seoService.setMetaOgUrl("https://herotraveldn.com/");
-        this._seoService.setMetaOgImage("https://herotraveldn.com/assets/img/logo/logo.png");
-        this._seoService.setMetaTwitterImage("https://herotraveldn.com/assets/img/logo/logo.png");
-        this._seoService.updateCanonicalUrl("https://herotraveldn.com/");
+        this.isBrowser = isPlatformBrowser(this._platformId);
+
+        this._seoService.setMetaTitle(
+            'Herotraveldn - Dịch vụ du lịch Huế - Đà Nẵng - Hội An'
+        );
+        this._seoService.setMetaDescription(
+            'Herotraveldn Chuyên tổ chức các Tour du lịch Miền Trung, Du lịch Đà Nẵng, Tour Du lịch hằng ngày Bà Nà, Hội An, Cù Lao Chàm.'
+        );
+        this._seoService.setMetaOgTitle(
+            'Herotraveldn - Dịch vụ du lịch Huế - Đà Nẵng - Hội An'
+        );
+        this._seoService.setMetaOgDescription(
+            'Herotraveldn Chuyên tổ chức các Tour du lịch Miền Trung, Du lịch Đà Nẵng, Tour Du lịch hằng ngày Bà Nà, Hội An, Cù Lao Chàm.'
+        );
+        this._seoService.setMetaTwitterTitle(
+            'Herotraveldn - Dịch vụ du lịch Huế - Đà Nẵng - Hội An'
+        );
+        this._seoService.setMetaTwitterDescription(
+            'Herotraveldn Chuyên tổ chức các Tour du lịch Miền Trung, Du lịch Đà Nẵng, Tour Du lịch hằng ngày Bà Nà, Hội An, Cù Lao Chàm.'
+        );
+        this._seoService.setMetaOgUrl('https://herotraveldn.com/');
+        this._seoService.setMetaOgImage(
+            'https://herotraveldn.com/assets/img/logo/logo.png'
+        );
+        this._seoService.setMetaTwitterImage(
+            'https://herotraveldn.com/assets/img/logo/logo.png'
+        );
+        this._seoService.updateCanonicalUrl('https://herotraveldn.com/');
+
+        if (this.isBrowser) {
+            const container = document.getElementById('homeBanner');
+            if (container !== null && container !== undefined) {
+                const swiper = new Swiper(container, {
+                    loop: true,
+                    pagination: {
+                        el: ".swiper-pagination",
+                        clickable: true,
+                        renderBullet: function (index, className) {
+                        return '<span class="' + className + '">' + (index + 1) + "</span>";
+                        },
+                    },
+                    autoplay: {
+                        delay: 2500,
+                        disableOnInteraction: false
+                    },
+                    modules: [Pagination, Autoplay]
+                });
+            }
+        }
     }
 }
